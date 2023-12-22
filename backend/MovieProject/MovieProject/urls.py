f5d7937acc9ad909d254d7706e0e18a9350187c3
johnpennyman95@gmail.com <john.pennyman@okstate.edu>
2023-12-22 00:30:36 -0600

    Add Authentication

--- backend/MovieProject/MovieProject/urls.py
+++ backend/MovieProject/MovieProject/urls.py
@@ -6,10 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('api.urls')),
-    path('',include('MovieApp.urls'))
-    # path('',include('events.urls')),
-    # path('members/',include('django.contrib.auth.urls')),
-    # path('members/',include('members.urls')),
+    path('',include('MovieApp.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
