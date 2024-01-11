9b702f1265102d484430e639c3197892687cfaf2
MyName <johnpennyman95@gmail.com>
2024-01-11 16:10:20 -0600

    prepare for deployment

--- backend/MovieProject/MovieProject/wsgi.py
+++ backend/MovieProject/MovieProject/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for MovieProject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MovieProject.settings')
+settings_module = 'MovieProject.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'MovieProject.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
--- backend/MovieProject/manage.py
+++ backend/MovieProject/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MovieProject.settings')
+    settings_module = 'MovieProject.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'MovieProject.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
