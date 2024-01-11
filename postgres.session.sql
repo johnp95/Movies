c1a9e3b6ca2acc7064fafd7c3b6d41b9beeb2a41
MyName <johnpennyman95@gmail.com>
2024-01-11 17:18:51 -0600

    removie my_venv

---
+++ postgres.session.sql
@@ -0,0 +1,12 @@
+insert into "MovieApp_movie" 
+(id,title, director,actor,actress,date_watched,released)
+VALUES (
+    1101,
+    'The Three Faces of Eve',
+    'Nunnally Johnson',
+    'David Wayne',
+    'Joanne Woodward',
+    '2023-12-03',
+    '1957'
+);
+
