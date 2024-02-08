693515f5ddd485cb76f9c53bd4d7a2a80febeac5
MyName <johnpennyman95@gmail.com>
2024-02-08 14:32:11 -0600

    redeploy

--- backend/MovieProject/deployment.py
+++ backend/MovieProject/deployment.py
@@ -35,7 +35,6 @@ STORAGES = {
 CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 CONNECTION_STR = {pair.split('=')[0]:pair.split('=')[1] for pair in CONNECTION.split(' ')}
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
