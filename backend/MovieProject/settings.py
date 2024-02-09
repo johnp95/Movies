8158e30f5d26194ac47e49264aaddb40651dd6ae
MyName <johnpennyman95@gmail.com>
2024-02-09 13:39:42 -0600

    ready for deployment

--- backend/MovieProject/settings.py
+++ backend/MovieProject/settings.py
@@ -1,20 +1,13 @@
 from pathlib import Path
 import os
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-lqf5=q*-w*wl94c-agfdi!mg@_k(-(=8abj-x#oicm_l*8*#_f'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['ashy-cliff-0dda47b10.4.azurestaticapps.net']
-
 
 # Application definition
 INSTALLED_APPS = [
