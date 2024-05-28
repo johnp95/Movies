0e709581ac2cf82ff6e495339955da564e0d1314
John Pennyman <johnpennyman95@gmail.com>
2024-05-28 12:19:45 -0500

    fix indentaion

--- frontend/src/components/InnerContainer.jsx
+++ frontend/src/components/InnerContainer.jsx
@@ -1,10 +1,5 @@
-import React from 'react';
-import styles from './styles/innercontainer.module.css';
+import styles from "./styles/innercontainer.module.css";
 
 export const InnerContainer = ({ children }) => {
-    return (
-        <div className={styles.innerContainer}>
-            {children}           
-        </div>
-    )
-}
+    return <div className={styles.innerContainer}>{children}</div>;
+};
