612a24bede3ff342426c4e25d4c672442649feb4
John Pennyman <johnpennyman95@gmail.com>
2024-05-28 12:16:24 -0500

    indent file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>
+    <Router>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Router>
 );
-
-
