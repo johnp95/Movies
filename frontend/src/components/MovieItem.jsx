600859392b8a04b0017e55580bcb8ba14d4033bc
John Pennyman <johnpennyman95@gmail.com>
2024-05-28 12:20:30 -0500

    fix indentation

--- frontend/src/components/MovieItem.jsx
+++ frontend/src/components/MovieItem.jsx
@@ -1,28 +1,28 @@
-import { React } from 'react';
-import { Link } from 'react-router-dom';
-import Button from '@mui/material/Button';
-import styles from './styles/movieitem.module.css';
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import styles from "./styles/movieitem.module.css";
 
 export const MovieItem = ({ movie, setMovieId }) => {
-
-  return (
-    <div className={styles.itemContainer}>
-      <img className={styles.itemImage} src={movie.image} alt='' />
-      <div className={styles.itemContent}>
-        <p className={styles.itemName}>{movie.title}</p>
-      </div>
-      <div className={styles.buttonContainer}>
-        <Link to={`/movie_details/${movie.id}`}>
-          <Button onClick={() => {
-            setMovieId(movie.id)
-            }} 
-            style={{backgroundColor:'#000d33', color:'white'}}
-            size='large'
-            variant='outlined'
-          >View Movie
-          </Button>
-        </Link>
-      </div>
-    </div>
-  )
-}
+    return (
+        <div className={styles.itemContainer}>
+            <img className={styles.itemImage} src={movie.image} alt="" />
+            <div className={styles.itemContent}>
+                <p className={styles.itemName}>{movie.title}</p>
+            </div>
+            <div className={styles.buttonContainer}>
+                <Link to={`/movie_details/${movie.id}`}>
+                    <Button
+                        onClick={() => {
+                            setMovieId(movie.id);
+                        }}
+                        style={{ backgroundColor: "#000d33", color: "white" }}
+                        size="large"
+                        variant="outlined"
+                    >
+                        View Movie
+                    </Button>
+                </Link>
+            </div>
+        </div>
+    );
+};
