698902cbe305fafffc5e47ea30f0a78b062baefe
John Pennyman <johnpennyman95@gmail.com>
2024-05-28 12:17:57 -0500

    fix indentation

--- frontend/src/components/AddMovie.jsx
+++ frontend/src/components/AddMovie.jsx
@@ -1,112 +1,111 @@
-import { React } from 'react';
-import { Box, Button, Typography } from '@mui/material';
-import MyDatePickerField from './forms/MyDatePickerField';
-import MyTextField from './forms/MyTextField';
-import { useForm } from 'react-hook-form';
-import AxiosInstance from './Axios';
-import { useNavigate } from 'react-router-dom';
-import { yupResolver } from '@hookform/resolvers/yup'
-import * as yup from 'yup';
-import styles from './styles/addmovie.module.css';
+import { Box, Button, Typography } from "@mui/material";
+import MyDatePickerField from "./forms/MyDatePickerField";
+import MyTextField from "./forms/MyTextField";
+import { useForm } from "react-hook-form";
+import AxiosInstance from "./Axios";
+import { useNavigate } from "react-router-dom";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import styles from "./styles/addmovie.module.css";
 
 export const AddMovie = () => {
-  
-  const navigate = useNavigate();
-  const defaultValues = {
-    title: '',
-    director: '',
-    actor: '',
-    actress: '',
-    date_watched: '',
-    released: '',
-    image: '',
-  };
+    const navigate = useNavigate();
+    const defaultValues = {
+        title: "",
+        director: "",
+        actor: "",
+        actress: "",
+        date_watched: "",
+        released: "",
+        image: "",
+    };
 
-  const schema = yup
-  .object({
-    title: yup.string().required('Title is required field'),
-    director: yup.string().required('Director is required field'),
-    actor: yup.string(),
-    actress: yup.string(),
-    date_watched: yup.date().required('Date Watched is required field'),
-    released: yup.string().required('Released is required field'),
-    image: yup.string(),
-  })
-  .required()
+    const schema = yup
+        .object({
+            title: yup.string().required("Title is required field"),
+            director: yup.string().required("Director is required field"),
+            actor: yup.string(),
+            actress: yup.string(),
+            date_watched: yup.date().required("Date Watched is required field"),
+            released: yup.string().required("Released is required field"),
+            image: yup.string(),
+        })
+        .required();
 
-  const {handleSubmit,control} = useForm({defaultValues:defaultValues,resolver: yupResolver(schema)})
+    const { handleSubmit, control } = useForm({
+        defaultValues: defaultValues,
+        resolver: yupResolver(schema),
+    });
     const submission = (data) => {
-      const date_watched = data.date_watched.toISOString().split('T')[0];
+        const date_watched = data.date_watched.toISOString().split("T")[0];
         AxiosInstance.post(`api/movies/`, {
-          title: data.title,
-          director: data.director,
-          actor: data.actor,
-          actress: data.actress,
-          date_watched: date_watched,
-          released: data.released,
-          image: data.image,
-        }).then((res) => {
-          navigate(`/`);
+            title: data.title,
+            director: data.director,
+            actor: data.actor,
+            actress: data.actress,
+            date_watched: date_watched,
+            released: data.released,
+            image: data.image,
+        }).then(() => {
+            navigate(`/`);
         });
     };
 
-  return (
-    <div className={styles.formContainer}>
-      <form onSubmit={handleSubmit(submission)}>
-        <Box className={styles.title}>
-          <Typography sx={{color:'#fff'}}>
-            Enter Movie
-          </Typography>
-        </Box>
-        <Box className={styles.textContainer}>
-            <MyTextField
-              label="Title"
-              name="title"
-              control={control}
-              placeholder="Provide a title"
-            />
-            <MyTextField
-              label="Director"
-              name="director"
-              control={control}
-              placeholder="Provide a director"
-            />
-            <MyTextField
-              label="Actor"
-              name="actor"
-              control={control}
-              placeholder="Provide an actor"
-            />
-            <MyTextField
-              label="Actress"
-              name="actress"
-              control={control}
-              placeholder="Provide an actress"
-            />
-            <MyDatePickerField
-              label="Date Watched"
-              name="date_watched"
-              control={control}
-            />
-            <MyTextField
-              label="Released"
-              name="released"
-              control={control}
-              placeholder="Provide released date"
-            />
-            <MyTextField
-              label="Image"
-              name="image"
-              control={control}
-              placeholder="Provide image"
-            />
-          <Box> 
-            <Button variant="contained" type="submit" size='large' >
-              Submit Movie
-            </Button>
-          </Box>
-        </Box>
-      </form> 
-    </div>
-  );
+    return (
+        <div className={styles.formContainer}>
+            <form onSubmit={handleSubmit(submission)}>
+                <Box className={styles.title}>
+                    <Typography sx={{ color: "#fff" }}>Enter Movie</Typography>
+                </Box>
+                <Box className={styles.textContainer}>
+                    <MyTextField
+                        label="Title"
+                        name="title"
+                        control={control}
+                        placeholder="Provide a title"
+                    />
+                    <MyTextField
+                        label="Director"
+                        name="director"
+                        control={control}
+                        placeholder="Provide a director"
+                    />
+                    <MyTextField
+                        label="Actor"
+                        name="actor"
+                        control={control}
+                        placeholder="Provide an actor"
+                    />
+                    <MyTextField
+                        label="Actress"
+                        name="actress"
+                        control={control}
+                        placeholder="Provide an actress"
+                    />
+                    <MyDatePickerField
+                        label="Date Watched"
+                        name="date_watched"
+                        control={control}
+                    />
+                    <MyTextField
+                        label="Released"
+                        name="released"
+                        control={control}
+                        placeholder="Provide released date"
+                    />
+                    <MyTextField
+                        label="Image"
+                        name="image"
+                        control={control}
+                        placeholder="Provide image"
+                    />
+                    <Box>
+                        <Button variant="contained" type="submit" size="large">
+                            Submit Movie
+                        </Button>
+                    </Box>
+                </Box>
+            </form>
+        </div>
+    );
 };
