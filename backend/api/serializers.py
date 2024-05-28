a22e1f6e24a6c2f074f86b8ba199e2356a6d3216
John Pennyman <johnpennyman95@gmail.com>
2024-05-28 12:13:51 -0500

    remove comment

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,5 +4,4 @@ from MovieApp.models import Movie
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        # fields = ('id','title','director','actor','actress','date_watched','released')
         fields = '__all__'
