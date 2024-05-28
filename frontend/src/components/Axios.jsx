edccd029bf4fafda70615620ba6b4d555bd1c8d7
John Pennyman <johnpennyman95@gmail.com>
2024-05-28 12:18:04 -0500

    fix indentation

--- frontend/src/components/Axios.jsx
+++ frontend/src/components/Axios.jsx
@@ -1,7 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
-const isDevelopment = import.meta.env.MODE === 'development'
-const baseUrl2 = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_PROD
+const isDevelopment = import.meta.env.MODE === "development";
+const baseUrl2 = isDevelopment
+    ? import.meta.env.VITE_API_BASE_URL_LOCAL
+    : import.meta.env.VITE_API_BASE_URL_PROD;
 
 const AxiosInstance = axios.create({
     baseURL: baseUrl2,
@@ -9,7 +11,7 @@ const AxiosInstance = axios.create({
     headers: {
         "Content-Type": "application/json",
         accept: "application/json",
-    }
-})
+    },
+});
 
 export default AxiosInstance;
