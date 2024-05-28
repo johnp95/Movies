df22779be8ba9ec26144c75ad18c123238bac675
John Pennyman <johnpennyman95@gmail.com>
2024-05-28 12:17:40 -0500

    remove log and add actress to useEffect

--- frontend/src/components/ActressDetail.jsx
+++ frontend/src/components/ActressDetail.jsx
@@ -1,50 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { MovieItem } from './MovieItem';
-import { Typography } from '@mui/material';
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
-import styles from './styles/directordetail.module.css';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { MovieItem } from "./MovieItem";
+import { Typography } from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import styles from "./styles/directordetail.module.css";
 
 export const ActressDetail = () => {
-
     const [movies, setMovies] = useState([]);
-    const [loading,setLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
     const myParam = useParams();
     const actress = myParam.id;
-    const url = 'http://127.0.0.1:8000/api/movies/actress';
+    const url = "http://127.0.0.1:8000/api/movies/actress";
 
     useEffect(() => {
         const fetchMovie = async () => {
             const res = await fetch(`${url}?actress=${actress}`);
             const data = await res.json();
-            console.log(data);
             setMovies(data);
             setLoading(false);
         };
-    fetchMovie();
-  }, []);
+        fetchMovie();
+    }, [actress]);
 
-  return (
-    <Box className={styles.container}>
-        <Typography 
-            variant='h3' 
-            fontWeight='bold'>
-            {actress}  Movies Seen: {movies.length}
-       </Typography>
-        {loading ?
-            <Box sx={{ display: 'flex' }}>
-                <CircularProgress />
-            </Box>
-            :
-            <div className={styles.cardContainer}>
-                {movies.map((movie) => (
-                    <MovieItem 
-                        key={movie.id} 
-                        movie={movie} /> 
+    return (
+        <Box className={styles.container}>
+            <Typography variant="h3" fontWeight="bold">
+                {actress} Movies Seen: {movies.length}
+            </Typography>
+            {loading ? (
+                <Box sx={{ display: "flex" }}>
+                    <CircularProgress />
+                </Box>
+            ) : (
+                <div className={styles.cardContainer}>
+                    {movies.map((movie) => (
+                        <MovieItem key={movie.id} movie={movie} />
                     ))}
-        </div>
-        }
-   </Box>
-  );
+                </div>
+            )}
+        </Box>
+    );
 };
