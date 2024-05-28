cf359f011a8795a43f763884bfada3803d950be4
John Pennyman <johnpennyman95@gmail.com>
2024-05-28 12:18:20 -0500

    fix indentation

--- frontend/src/components/Container.jsx
+++ frontend/src/components/Container.jsx
@@ -1,11 +1,5 @@
-import React from 'react';
-import styles from './styles/container.module.css';
+import styles from "./styles/container.module.css";
 
 export const Container = ({ children }) => {
-    
-    return (
-        <div className={styles.parentContainer}>
-            {children}
-        </div>
-    )
-}
+    return <div className={styles.parentContainer}>{children}</div>;
+};
