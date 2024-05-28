0bee2494da40ac20f87e88e89bfb1a254a1173d6
John Pennyman <johnpennyman95@gmail.com>
2024-05-28 12:18:14 -0500

    fix indentation

--- frontend/src/components/BestPicture.jsx
+++ frontend/src/components/BestPicture.jsx
@@ -1,27 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { MovieItem } from './MovieItem';
-import styles from './styles/bestpicture.module.css';
+import { useState, useEffect } from "react";
+import { MovieItem } from "./MovieItem";
+import styles from "./styles/bestpicture.module.css";
 
-const url = 'http://127.0.0.1:8000/api/movies/';
+const url = "http://127.0.0.1:8000/api/movies/";
 
 export const BestPicture = () => {
-  const [movies, setMovies] = useState([])
-  useEffect(() => {
-    const fetchMovie = async () => {
-      const res = await fetch(`${url}`);
-      const data = await res.json();
-      console.log(data);
-      setMovies(data);
-    };
-    fetchMovie();
-  }, []);
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        const fetchMovie = async () => {
+            const res = await fetch(`${url}`);
+            const data = await res.json();
+            setMovies(data);
+        };
+        fetchMovie();
+    }, []);
 
-  return (
-    <div className={styles.cardContainer}>
-      {movies.map((movie) => (
-        movie.best_picture ?
-        <MovieItem key={movie.id} movie={movie}/>  : ''
-      ))}    
-    </div>
-  );
+    return (
+        <div className={styles.cardContainer}>
+            {movies.map((movie) =>
+                movie.best_picture ? (
+                    <MovieItem key={movie.id} movie={movie} />
+                ) : (
+                    ""
+                )
+            )}
+        </div>
+    );
 };
