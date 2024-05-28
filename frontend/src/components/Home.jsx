47c15a4a2383188147720dd5456084d57cceb67b
John Pennyman <johnpennyman95@gmail.com>
2024-05-28 12:19:38 -0500

    fix indentaion

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,93 +1,103 @@
-import {React, useEffect, useMemo,useState} from 'react';
-import AxiosInstance from './Axios';
-import {MaterialReactTable} from 'material-react-table';
-import Dayjs from 'dayjs';
-import { Box, IconButton } from '@mui/material';
-import { Edit as EditIcon, Delete as DeleteIcon} from '@mui/icons-material';
-import {Link} from 'react-router-dom';
-import CircularProgress from '@mui/material/CircularProgress';
+import { useEffect, useMemo, useState } from "react";
+import AxiosInstance from "./Axios";
+import { MaterialReactTable } from "material-react-table";
+import Dayjs from "dayjs";
+import { Box, IconButton } from "@mui/material";
+import { Edit as EditIcon, Delete as DeleteIcon } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export const Home = () => {
+    const [myData, setMyData] = useState();
+    const [loading, setLoading] = useState(true);
 
-  const [myData,setMyData] = useState();
-  const [loading,setLoading] = useState(true);
+    const GetData = () => {
+        AxiosInstance.get(`api/movies/`).then((res) => {
+            setMyData(res.data);
+            setLoading(false);
+        });
+    };
 
-  const GetData = () => {
-    AxiosInstance.get(`api/movies/`).then((res) => {
+    useEffect(() => {
+        GetData();
+    }, []);
 
-      setMyData(res.data);
-      console.log(res.data);
-      setLoading(false);
-    })
-  }
-
-  useEffect(() => {
-    GetData();
-
-  },[])
- 
     const columns = useMemo(
-      () => [
-        {
-          accessorKey: 'title', 
-          header: 'Title',
-          size: 150,
-        },
-        {
-          accessorKey: 'released', 
-          header: 'Released',
-          size: 150,
-        },
-        {
-          accessorKey: 'director', 
-          header: 'Director',
-          size: 150,
-        },
-        {
-          accessorKey: 'actor', 
-          header: 'Actor',
-          size: 150,
-        },
-        {
-          accessorKey: 'actress', 
-          header: 'Actress',
-          size: 150,
-        },
-       
-        {
-          accessorFn: (row) => Dayjs(row.date_watched).format('YYYY-MM-DD'),
-          header: 'Date Watched',
-          size: 150,
-        },
- 
-      ],
-      [],
-    );
-  
-  return (
-    <div>
-      { loading ? 
-        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-          <CircularProgress />
-        </Box>
-         : 
-        <MaterialReactTable 
-          columns={columns}
-          data={myData} 
-          enableRowActions
-          renderRowActions={({row}) => (
-            <Box sx={{ display: 'flex', flexWrap: 'nowrap', gap: '8px' }}>
-              <IconButton color="secondary" component={Link} to={`edit/${row.original.id}`}>
-                <EditIcon />
-              </IconButton>
+        () => [
+            {
+                accessorKey: "title",
+                header: "Title",
+                size: 150,
+            },
+            {
+                accessorKey: "released",
+                header: "Released",
+                size: 150,
+            },
+            {
+                accessorKey: "director",
+                header: "Director",
+                size: 150,
+            },
+            {
+                accessorKey: "actor",
+                header: "Actor",
+                size: 150,
+            },
+            {
+                accessorKey: "actress",
+                header: "Actress",
+                size: 150,
+            },
 
-              <IconButton color="error" component={Link} to={`delete/${row.original.id}`}>
-                <DeleteIcon />
-              </IconButton>
-            </Box>
-          )}
-         />
-      }
-    </div>
-  )
-}
+            {
+                accessorFn: (row) =>
+                    Dayjs(row.date_watched).format("YYYY-MM-DD"),
+                header: "Date Watched",
+                size: 150,
+            },
+        ],
+        []
+    );
+
+    return (
+        <div>
+            {loading ? (
+                <Box sx={{ display: "flex", justifyContent: "center" }}>
+                    <CircularProgress />
+                </Box>
+            ) : (
+                <MaterialReactTable
+                    columns={columns}
+                    data={myData}
+                    enableRowActions
+                    renderRowActions={({ row }) => (
+                        <Box
+                            sx={{
+                                display: "flex",
+                                flexWrap: "nowrap",
+                                gap: "8px",
+                            }}
+                        >
+                            <IconButton
+                                color="secondary"
+                                component={Link}
+                                to={`edit/${row.original.id}`}
+                            >
+                                <EditIcon />
+                            </IconButton>
+
+                            <IconButton
+                                color="error"
+                                component={Link}
+                                to={`delete/${row.original.id}`}
+                            >
+                                <DeleteIcon />
+                            </IconButton>
+                        </Box>
+                    )}
+                />
+            )}
+        </div>
+    );
+};
