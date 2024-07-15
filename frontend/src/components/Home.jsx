cf1a6713b873514a01e89822ebaa8ac621b24f4f
John Pennyman <johnpennyman95@gmail.com>
2024-07-15 12:53:50 -0500

    utilize asynchonous data fetching

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useMemo, useState } from "react";
-import AxiosInstance from "./Axios";
 import { MaterialReactTable } from "material-react-table";
 import Dayjs from "dayjs";
 import { Box, IconButton } from "@mui/material";
@@ -7,19 +6,25 @@ import { Edit as EditIcon, Delete as DeleteIcon } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import CircularProgress from "@mui/material/CircularProgress";
 
+const url = "http://127.0.0.1:8000/api/movies";
+
 export const Home = () => {
-    const [myData, setMyData] = useState();
+    const [myData, setMyData] = useState([]);
     const [loading, setLoading] = useState(true);
 
-    const GetData = () => {
-        AxiosInstance.get(`api/movies/`).then((res) => {
-            setMyData(res.data);
-            setLoading(false);
-        });
-    };
-
     useEffect(() => {
-        GetData();
+        const fetchMovies = async () => {
+            try {
+                const res = await fetch(url);
+                const data = await res.json();
+                setMyData(data);
+            } catch (error) {
+                console.log("Error fetching data", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchMovies();
     }, []);
 
     const columns = useMemo(
