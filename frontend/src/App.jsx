23d2daac23008cfba6846c08a520b6488eb6de27
John Pennyman <johnpennyman95@gmail.com>
2024-07-22 13:02:32 -0500

    implement current method to use router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,32 @@
-import { Routes, Route } from "react-router-dom";
-import { Home } from "./components/Home";
-import { AddMovie } from "./components/AddMovie";
-import { Navbar } from "./components/Navbar";
-import { Edit } from "./components/Edit";
-import { Delete } from "./components/Delete";
+import {
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements,
+    RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./Layouts/MainLayout";
+import HomePage from "./Pages/HomePage";
 import { SearchComponent } from "./components/SearchComponent";
-import { MovieDetails } from "./components/MovieDetails";
+import { AddMovie } from "./components/AddMovie";
+import NotFoundPage from "./Pages/NotFoundPage";
 import { BestPicture } from "./components/BestPicture";
-import { DirectorDetail } from "./components/DirectorDetail";
-import { ActorDetail } from "./components/ActorDetail";
-import { ActressDetail } from "./components/ActressDetail";
+import { MovieDetails } from "./components/MovieDetails";
 
-function App() {
-    return (
-        <div className="App">
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/search" element={<SearchComponent />} />
-                <Route path="/add_movie" element={<AddMovie />} />
-                <Route path="/edit/:id" element={<Edit />} />
-                <Route path="/delete/:id" element={<Delete />} />
-                <Route path="/movie_details/:id" element={<MovieDetails />} />
-                <Route
-                    path="/director_detail/:id"
-                    element={<DirectorDetail />}
-                />
-                <Route path="/actor_detail/:id" element={<ActorDetail />} />
-                <Route path="/actress_detail/:id" element={<ActressDetail />} />
-                <Route path="/best_picture" element={<BestPicture />} />
-            </Routes>
-        </div>
-    );
-}
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<MainLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="search" element={<SearchComponent />} />
+            <Route path="/movie_details/:id" element={<MovieDetails />} />
+            <Route path="/add_movie" element={<AddMovie />} />
+            <Route path="/best_picture" element={<BestPicture />} />
+            <Route path="*" element={<NotFoundPage />} />
+        </Route>
+    )
+);
+
+const App = () => {
+    return <RouterProvider router={router} />;
+};
 
 export default App;
