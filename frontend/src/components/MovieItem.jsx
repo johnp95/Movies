1e2d63f68072f5176d525108b968e0a5c39b9380
John Pennyman <johnpennyman95@gmail.com>
2024-07-22 23:00:55 -0500

    add tailwind to movie item

--- frontend/src/components/MovieItem.jsx
+++ frontend/src/components/MovieItem.jsx
@@ -1,28 +1,29 @@
 import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
-import styles from "./styles/movieitem.module.css";
 
-export const MovieItem = ({ movie, setMovieId }) => {
+const MovieItem = ({ movie, setMovieId }) => {
     return (
-        <div className={styles.itemContainer}>
-            <img className={styles.itemImage} src={movie.image} alt="" />
-            <div className={styles.itemContent}>
-                <p className={styles.itemName}>{movie.title}</p>
+        <div className="w-64 rounded-lg shadow-md m-5 overflow-hidden font-sans">
+            <img
+                className="w-64 h-[290px] object-cover"
+                src={movie.image}
+                alt={movie.title}
+            />
+            <div className="text-center font-arial">
+                <p className="text-base font-semibold text-gray-700 mt-2">
+                    {movie.title}
+                </p>
             </div>
-            <div className={styles.buttonContainer}>
+            <div className="mb-5 flex justify-around text-center mt-4">
                 <Link to={`/movie_details/${movie.id}`}>
-                    <Button
-                        onClick={() => {
-                            setMovieId(movie.id);
-                        }}
-                        style={{ backgroundColor: "#000d33", color: "white" }}
-                        size="large"
-                        variant="outlined"
+                    <button
+                        onClick={() => setMovieId(movie.id)}
+                        className="bg-[#000d33] text-white px-4 py-2 rounded hover:bg-[#001a66] transition duration-300"
                     >
                         View Movie
-                    </Button>
+                    </button>
                 </Link>
             </div>
         </div>
     );
 };
+export default MovieItem;
