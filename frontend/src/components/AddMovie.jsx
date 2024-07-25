b8ab029af01dd4a12f3fad2f1dd6aaef304d201a
John Pennyman <johnpennyman95@gmail.com>
2024-07-24 23:53:16 -0500

    remove yup/mui etc

--- frontend/src/components/AddMovie.jsx
+++ frontend/src/components/AddMovie.jsx
@@ -1,112 +1,193 @@
-import { Box, Button, Typography } from "@mui/material";
-import MyDatePickerField from "./forms/MyDatePickerField";
-import MyTextField from "./forms/MyTextField";
-import { useForm } from "react-hook-form";
-import AxiosInstance from "./Axios";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import styles from "./styles/addmovie.module.css";
+
+const url = import.meta.env.VITE_API_BASE_URL_LOCAL + `api/movies/`;
 
 const AddMovie = () => {
+    const [title, setTitle] = useState("");
+    const [director, setDirector] = useState("");
+    const [actor, setActor] = useState("");
+    const [actress, setActress] = useState("");
+    const [dateWatched, setDateWatched] = useState("");
+    const [released, setReleased] = useState("");
+    const [image, setImage] = useState("");
+
     const navigate = useNavigate();
-    const defaultValues = {
-        title: "",
-        director: "",
-        actor: "",
-        actress: "",
-        date_watched: "",
-        released: "",
-        image: "",
+    const addMovie = async (newMovie) => {
+        try {
+            const res = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(newMovie),
+            });
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            const data = await res.json();
+            console.log("Movie added successfully:", data);
+        } catch (error) {
+            console.error("Error adding movie:", error);
+        }
     };
+    const submitForm = (e) => {
+        e.preventDefault();
 
-    const schema = yup
-        .object({
-            title: yup.string().required("Title is required field"),
-            director: yup.string().required("Director is required field"),
-            actor: yup.string(),
-            actress: yup.string(),
-            date_watched: yup.date().required("Date Watched is required field"),
-            released: yup.string().required("Released is required field"),
-            image: yup.string(),
-        })
-        .required();
-
-    const { handleSubmit, control } = useForm({
-        defaultValues: defaultValues,
-        resolver: yupResolver(schema),
-    });
-    const submission = (data) => {
-        const date_watched = data.date_watched.toISOString().split("T")[0];
-        AxiosInstance.post(`api/movies/`, {
-            title: data.title,
-            director: data.director,
-            actor: data.actor,
-            actress: data.actress,
-            date_watched: date_watched,
-            released: data.released,
-            image: data.image,
-        }).then(() => {
-            navigate(`/`);
-        });
+        const newMovie = {
+            title,
+            director,
+            actor,
+            actress,
+            date_watched: dateWatched,
+            released: Number(released),
+            image,
+        };
+        addMovie(newMovie);
+        navigate("/");
     };
 
     return (
-        <div className={styles.formContainer}>
-            <form onSubmit={handleSubmit(submission)}>
-                <Box className={styles.title}>
-                    <Typography sx={{ color: "#fff" }}>Enter Movie</Typography>
-                </Box>
-                <Box className={styles.textContainer}>
-                    <MyTextField
-                        label="Title"
-                        name="title"
-                        control={control}
-                        placeholder="Provide a title"
-                    />
-                    <MyTextField
-                        label="Director"
-                        name="director"
-                        control={control}
-                        placeholder="Provide a director"
-                    />
-                    <MyTextField
-                        label="Actor"
-                        name="actor"
-                        control={control}
-                        placeholder="Provide an actor"
-                    />
-                    <MyTextField
-                        label="Actress"
-                        name="actress"
-                        control={control}
-                        placeholder="Provide an actress"
-                    />
-                    <MyDatePickerField
-                        label="Date Watched"
-                        name="date_watched"
-                        control={control}
-                    />
-                    <MyTextField
-                        label="Released"
-                        name="released"
-                        control={control}
-                        placeholder="Provide released date"
-                    />
-                    <MyTextField
-                        label="Image"
-                        name="image"
-                        control={control}
-                        placeholder="Provide image"
-                    />
-                    <Box>
-                        <Button variant="contained" type="submit" size="large">
-                            Submit Movie
-                        </Button>
-                    </Box>
-                </Box>
-            </form>
-        </div>
+        <>
+            <section className="bg-indigo-50">
+                <div className="container m-auto max-w-2xl py-24">
+                    <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+                        <form onSubmit={submitForm}>
+                            <h2 className="text-3xl text-center font-semibold mb-6">
+                                Add Movie
+                            </h2>
+
+                            <div className="mb-4">
+                                <label className="block text-gray-700 font-bold mb-2">
+                                    Title
+                                </label>
+                                <input
+                                    type="text"
+                                    id="title"
+                                    name="title"
+                                    className="border rounded w-full py-2 px-3 mb-2"
+                                    onChange={(e) => {
+                                        setTitle(e.target.value);
+                                    }}
+                                    value={title}
+                                    placeholder="Title"
+                                    required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 font-bold mb-2">
+                                    Director
+                                </label>
+                                <input
+                                    type="text"
+                                    id="director"
+                                    name="director"
+                                    className="border rounded w-full py-2 px-3 mb-2"
+                                    onChange={(e) => {
+                                        setDirector(e.target.value);
+                                    }}
+                                    value={director}
+                                    placeholder="Director"
+                                    required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 font-bold mb-2">
+                                    Actor
+                                </label>
+                                <input
+                                    type="text"
+                                    id="actor"
+                                    name="actor"
+                                    className="border rounded w-full py-2 px-3 mb-2"
+                                    onChange={(e) => {
+                                        setActor(e.target.value);
+                                    }}
+                                    value={actor}
+                                    placeholder="Actor"
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 font-bold mb-2">
+                                    Actress
+                                </label>
+                                <input
+                                    type="text"
+                                    id="actress"
+                                    name="actress"
+                                    className="border rounded w-full py-2 px-3 mb-2"
+                                    onChange={(e) => {
+                                        setActress(e.target.value);
+                                    }}
+                                    value={actress}
+                                    placeholder="Actress"
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 font-bold mb-2">
+                                    Date Watched
+                                </label>
+                                <input
+                                    type="date"
+                                    id="dateWatched"
+                                    name="dateWatched"
+                                    className="border rounded w-full py-2 px-3 mb-2"
+                                    onChange={(e) => {
+                                        setDateWatched(e.target.value);
+                                    }}
+                                    value={dateWatched}
+                                    placeholder="Date Watched"
+                                    required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 font-bold mb-2">
+                                    Released
+                                </label>
+                                <input
+                                    type="number"
+                                    id="released"
+                                    name="released"
+                                    className="border rounded w-full py-2 px-3 mb-2"
+                                    onChange={(e) => {
+                                        setReleased(e.target.value);
+                                    }}
+                                    value={released}
+                                    placeholder="Released"
+                                    required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 font-bold mb-2">
+                                    Image
+                                </label>
+                                <input
+                                    type="text"
+                                    id="image"
+                                    name="image"
+                                    className="border rounded w-full py-2 px-3 mb-2"
+                                    onChange={(e) => {
+                                        setImage(e.target.value);
+                                    }}
+                                    value={image}
+                                    placeholder="Image"
+                                    required
+                                />
+                            </div>
+
+                            <div>
+                                <button
+                                    className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
+                                    type="submit"
+                                >
+                                    Add Movie
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </>
     );
 };
 
