a4fb898cad498119a64bca659728bc4c305bc923
John Pennyman <johnpennyman95@gmail.com>
2024-07-31 13:44:33 -0500

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,6 @@ import ActorDetail from "./components/ActorDetail";
 import ActressDetail from "./components/ActressDetail";
 import ForeignFilms from "./components/ForeignFilms";
 import OldestFilms from "./components/OldestFilms";
-
 import Delete from "./components/Delete";
 import Edit, { movieLoader } from "./components/Edit";
 
@@ -29,7 +28,6 @@ const router = createBrowserRouter(
             <Route path="/actor_detail/:id" element={<ActorDetail />} />
             <Route path="/delete/:id" element={<Delete />} />
             <Route path="/oldest_films" element={<OldestFilms />} />
-
             <Route path="/actress_detail/:id" element={<ActressDetail />} />
             <Route path="/director_detail/:id" element={<DirectorDetail />} />
             <Route path="/add_movie" element={<AddMovie />} />
